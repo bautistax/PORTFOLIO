1bbfd1431620937c23a3416e0542e0d1c15ca84b
bautistax <jbautistabauza@gmail.com>
2023-07-04 15:14:19 -0300

    Design

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+import img1 from '../assets/portfolio-img1.png';
 
 const Work = () => {
-  return <div className='section' id='work'>Work</div>;
+  return (
+    <section className='section' id='work'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col lg:flex-row gap-x-10'>
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+            {/* text */}
+            <div>
+              <h2 className='h2 leading-tight text-accent'>
+                My latest <br />
+                Work.
+              </h2>
+              <p className='max-w-sm mb-16'>
+                Lorem ipsum dolor sit amet. Sed nemo repellat qui distinctio quos et sequi cumque qui quos quas vel porro minima. Et excepturi molestias vel tenetur dolorum ut quaerat voluptates. Qui odit voluptatem non reprehenderit dolores eos harum doloribus ut tenetur deleniti ab quia ipsa.
+              </p>
+              <button className='btn btn-sm'>View all proyects</button>
+            </div>
+            {/* image */}
+            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+              {/* overlay */}
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              {/* img */}
+              <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt='' />
+              {/* pretitle */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                <span className='text-gradient'>Henry's Final Project</span>
+              </div>
+              {/* title */}
+              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                <span className='text-3x1 text-white'>
+                  BookBuster
+                </span>
+              </div>
+            </div>
+          </motion.div>
+          <motion.div variants={fadeIn('left', 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 flex flex-col gap-y-10'>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Work;

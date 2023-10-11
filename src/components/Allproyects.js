5b93c081cdb7d877f325750bd45972ac3604b104
bautistax <jbautistabauza@gmail.com>
2023-10-11 20:01:19 -0300

    Proyects section design

--- src/components/Allproyects.js
+++ src/components/Allproyects.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variants';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import img1 from '../assets/BookBuster.jpeg';
 import img2 from '../assets/bk-countries.jpg';
 import img3 from '../assets/bk-rickandmorty.jpg';
@@ -8,131 +9,111 @@ import img4 from '../assets/QR.jpg';
 import img5 from "../assets/weatherimage.png";
 
 const AllProyects = () => {
+  const projects = [
+    {
+      title: "BookBuster",
+      image: img1,
+      url: "https://book-buster-front-fhekz.vercel.app"
+    },
+    {
+      title: "Countries API",
+      image: img2,
+      url: "https://github.com/bautistax/PI-Countries-main"
+    },
+    {
+      title: "Rick & Morty API",
+      image: img3,
+      url: "https://github.com/bautistax/Rick-and-Morty"
+    },
+    {
+      title: "QR-component",
+      image: img4,
+      url: "https://github.com/bautistax/qr-code-component-main"
+    },
+    {
+      title: "WeatherApp",
+      image: img5,
+      url: "https://weatherappi.vercel.app/"
+    },
+  ];
+
+  const Project = ({ project }) => {
+    const handleClick = () => {
+      window.location.href = project.url;
+    };
+  
+    return (
+      <div
+        onClick={handleClick}
+        style={{ cursor: "pointer" }}
+        className="relative w-full h-96 max-w-md mx-auto mb-4 overflow-hidden group"
+      >
+        {/* overlay */}
+        <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+        
+        {/* img */}
+        <img
+          src={project.image}
+          alt=""
+          className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover rounded-lg'
+        />
+  
+        {/* title */}
+        <div className='absolute bottom-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-700 z-50'>
+          <span className='text-3xl text-white'>{project.title}</span>
+        </div>
+      </div>
+    );
+  };
+  
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <section className="section" id="proyects">
-      <div className="container mx-4 lg:mx-14">
+      <div className="container mx-auto lg:px-4 lg:mx-14">
         <div className="lg:flex-1 lg:ml-auto">
-          <motion.div
-            variants={fadeIn('right', 0.3)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.3 }}
-            className='flex-1 flex flex-col gap-y-12 mb-7 lg:mb-0'>
-            {/* text */}
-            <div className='mt-7 lg:mr-16'>
-              <h2 className='h2 leading-tight text-accent'>
+        <h2 className='flex h2 leading-tight text-accent mb-14 top-0'>
                 All&nbsp;&nbsp;Proyects
               </h2>
-            </div>
-          </motion.div>
-
-          <motion.div
-            variants={fadeIn('left', 0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.3 }}
-            className='flex-1 flex flex-col gap-y-5 lg:flex-row lg:flex-wrap justify-center'>
-
-            {/* image 1*/}
-            <a href='https://book-buster-front-fhekz.vercel.app'>
-              <div className='w-full lg:w-80 h-72 group relative overflow-hidden border-2 border-white/50 rounded-xl mx-3'>
-                {/* overlay */}
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                {/* img */}
-                <img className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover' src={img1} alt='' />
-                {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                  <span className='text-gradient'>Henry's Final Project</span>
-                </div>
-                {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                  <span className='text-3xl text-white'>
-                    BookBuster
-                  </span>
-                </div>
-              </div>
-            </a>
-
-            {/* image 2*/}
-            <a href='https://github.com/bautistax/PI-Countries-main'>
-              <div className='w-full lg:w-80 h-72 group relative overflow-hidden border-2 border-white/50 rounded-xl mx-3'>
-                {/* overlay */}
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                {/* img */}
-                <img className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover' src={img2} alt='' />
-                {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                  <span className='text-gradient'>Henry's PI Proyect</span>
-                </div>
-                {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                  <span className='text-3xl text-white'>
-                    Countries API
-                  </span>
-                </div>
-              </div>
-            </a>
-
-            {/* image 3*/}
-            <a href='https://github.com/bautistax/Rick-and-Morty'>
-              <div className='w-full lg:w-80 h-72 group relative overflow-hidden border-2 border-white/50 rounded-xl mx-3'>
-                {/* overlay */}
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                {/* img */}
-                <img className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover' src={img3} alt='' />
-                {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                  <span className='text-gradient'>Henry's Bootcamp Proyect</span>
-                </div>
-                {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                  <span className='text-3xl text-white'>
-                    Rick & Morty API
-                  </span>
-                </div>
-              </div>
-            </a>
-
-            {/* image 4*/}
-            <a href='https://github.com/bautistax/qr-code-component-main'>
-              <div className='w-full lg:w-80 h-72 group relative overflow-hidden border-2 border-white/50 rounded-xl mx-3'>
-                {/* overlay */}
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                {/* img */}
-                <img className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover' src={img4} alt='' />
-                {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                  <span className='text-gradient'>Personal Proyect</span>
-                </div>
-                {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                  <span className='text-3xl text-white'>
-                    QR-component
-                  </span>
-                </div>
-              </div>
-            </a>
-
-            {/* image 5*/}
-            <a href='https://weatherappi.vercel.app/'>
-              <div className='w-full lg:w-80 h-72 group relative overflow-hidden border-2 border-white/50 rounded-xl mx-3'>
-                {/* overlay */}
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                {/* img */}
-                <img className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover' src={img5} alt='' />
-                {/* pretitle */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                  <span className='text-gradient'>Personal Proyect</span>
-                </div>
-                {/* title */}
-                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                  <span className='text-3xl text-white'>
-                    WeatherApp
-                  </span>
-                </div>
-              </div>
-            </a>
-          </motion.div>
+          <Slider {...settings}>
+            {projects.map(project => (
+              <Project key={project.title} project={project} />
+            ))}
+          </Slider>
         </div>
       </div>
     </section>

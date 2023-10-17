bd014f079fca6ad3ba1a7eec887deb7751858e02
bautistax <jbautistabauza@gmail.com>
2023-10-17 18:06:31 -0300

    Responsive design

--- src/components/About.js
+++ src/components/About.js
@@ -1,54 +1,66 @@
-import React from 'react';
-import { Link } from 'react-scroll';
-import CountUp from 'react-countup';
-import { useInView } from 'react-intersection-observer';
-import { motion } from 'framer-motion';
-import { fadeIn } from '../variants';
+import React from "react";
+import { Link } from "react-scroll";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
   const [ref, inView] = useInView({
     threshold: 0.5,
   });
   return (
-    <section className='section' id='about' ref={ref}>
-      <div className='container mx-auto'>
-        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/* text */}
-          <motion.div variants={fadeIn('left', 0.5)}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: false, amount: 0.3 }}
-            className='flex-1'
+            className="flex-1"
           >
-            <h2 className='h2 text-accent'>About me.</h2>
-            <h3 className='h3 mb-4'>I'm a Front-end web developer with 1 year of experience </h3>
-            <p className='mb-6'>
-              I am a junior fullstack programmer focused on frontend programming. One of my biggest motivations is to learn new skills. I recently finished the course at Henry, where I had the opportunity to work on various projects, using technologies and frameworks such as Javascript, HMTL/CSS, React, Redux, Tailwind CSS, among others; and develop my skills in the world of software. My experience in Henry's projects allowed me to apply my knowledge and work in a team, which gave me a complete vision of software development. I now hope to take my skills to the next level and continue learning in a work environment.
+            <h2 className="text-4xl lg:text-5xl text-accent">About me.</h2>
+            <h3 className="text-xl lg:text-2xl mb-4">
+              I'm a Front-end web developer with 1 year of experience
+            </h3>
+            <p className="mb-6 text-sm lg:text-base">
+              I am a junior fullstack programmer focused on frontend
+              programming. One of my biggest motivations is to learn new skills.
+              I recently finished the course at Henry, where I had the
+              opportunity to work on various projects, using technologies and
+              frameworks such as Javascript, HMTL/CSS, React, Redux, Tailwind
+              CSS, among others; and develop my skills in the world of software.
+              My experience in Henry's projects allowed me to apply my knowledge
+              and work in a team, which gave me a complete vision of software
+              development. I now hope to take my skills to the next level and
+              continue learning in a work environment.
             </p>
             {/* stats */}
-            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
-              <div>
-                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+            <div className="flex flex-col lg:flex-row gap-y-6 lg:gap-y-0 lg:gap-x-10 mb-12">
+              <div className="text-center">
+                <div className="text-4xl lg:text-5xl font-tertiary text-gradient mb-2">
                   {inView ? <CountUp start={0} end={1} duration={7} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>
+                <div className="text-sm lg:text-base font-primary tracking-[2px]">
                   Years of <br />
                   Experience
                 </div>
               </div>
-              <div>
-                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+              <div className="text-center">
+                <div className="text-4xl lg:text-5xl font-tertiary text-gradient mb-2">
                   {inView ? <CountUp start={0} end={5} duration={8} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>
+                <div className="text-sm lg:text-base font-primary tracking-[2px]">
                   Projects <br />
                   Completed
                 </div>
               </div>
             </div>
-            <div className='flex gap-x-8 items-center'>
-              <Link to='contact'>
-                <button className='btn btn-lg'>Contact me</button>
+            <div className="flex items-center">
+              <Link to="contact">
+                <button className="btn btn-lg">Contact me</button>
               </Link>
             </div>
           </motion.div>
--- src/components/Allproyects.js
+++ src/components/Allproyects.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import img1 from '../assets/BookBuster.jpeg';
-import img2 from '../assets/bk-countries.jpg';
-import img3 from '../assets/bk-rickandmorty.jpg';
-import img4 from '../assets/QR.jpg';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import img1 from "../assets/BookBuster.jpeg";
+import img2 from "../assets/bk-countries.jpg";
+import img3 from "../assets/bk-rickandmorty.jpg";
+import img4 from "../assets/QR.jpg";
 import img5 from "../assets/weatherimage.png";
 
 const AllProyects = () => {
@@ -13,27 +13,27 @@ const AllProyects = () => {
     {
       title: "BookBuster",
       image: img1,
-      url: "https://book-buster-front-fhekz.vercel.app"
+      url: "https://book-buster-front-fhekz.vercel.app",
     },
     {
       title: "Countries API",
       image: img2,
-      url: "https://github.com/bautistax/PI-Countries-main"
+      url: "https://github.com/bautistax/PI-Countries-main",
     },
     {
       title: "Rick & Morty API",
       image: img3,
-      url: "https://github.com/bautistax/Rick-and-Morty"
+      url: "https://github.com/bautistax/Rick-and-Morty",
     },
     {
       title: "QR-component",
       image: img4,
-      url: "https://github.com/bautistax/qr-code-component-main"
+      url: "https://github.com/bautistax/qr-code-component-main",
     },
     {
       title: "WeatherApp",
       image: img5,
-      url: "https://weatherappi.vercel.app/"
+      url: "https://weatherappi.vercel.app/",
     },
   ];
 
@@ -41,7 +41,7 @@ const AllProyects = () => {
     const handleClick = () => {
       window.location.href = project.url;
     };
-  
+
     return (
       <div
         onClick={handleClick}
@@ -49,23 +49,22 @@ const AllProyects = () => {
         className="relative w-full h-96 max-w-md mx-auto mb-4 overflow-hidden group"
       >
         {/* overlay */}
-        <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-        
+        <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+
         {/* img */}
         <img
           src={project.image}
           alt=""
-          className='group-hover:scale-125 transition-all duration-500 w-full h-full object-cover rounded-lg'
+          className="group-hover:scale-125 transition-all duration-500 w-full h-full object-cover rounded-lg"
         />
-  
+
         {/* title */}
-        <div className='absolute bottom-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-700 z-50'>
-          <span className='text-3xl text-white'>{project.title}</span>
+        <div className="absolute bottom-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-700 z-50">
+          <span className="text-3xl text-white">{project.title}</span>
         </div>
       </div>
     );
   };
-  
 
   const settings = {
     dots: true,
@@ -82,38 +81,40 @@ const AllProyects = () => {
           slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
+          slidesToScroll: 1,
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <section className="section" id="proyects">
       <div className="container mx-auto lg:px-4 lg:mx-14">
         <div className="lg:flex-1 lg:ml-auto">
-        <h2 className='flex h2 leading-tight text-accent mb-14 top-0'>
-                All&nbsp;&nbsp;Proyects
-              </h2>
-          <Slider {...settings}>
-            {projects.map(project => (
-              <Project key={project.title} project={project} />
-            ))}
-          </Slider>
+          <h2 className="text-4xl lg:text-5xl leading-tight text-accent mb-6 lg:mb-14">
+            All&nbsp;&nbsp;Proyects
+          </h2>
+          <div>
+            <Slider {...settings}>
+              {projects.map((project) => (
+                <Project key={project.title} project={project} />
+              ))}
+            </Slider>
+          </div>
         </div>
       </div>
     </section>
